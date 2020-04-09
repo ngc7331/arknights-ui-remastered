cebd6cf6279482bb496769508fec60f800a45147
ngc7331 <ngc7331@outlook.com>
2020-04-09 15:16:02 +0800

    V0.2
    恢复了不小心删掉的normalize.css（我也不知道为啥dreamweaver一打开它就没了）
    修复了玩家昵称中文换行错误的问题
    增加了自定义昵称、ID和等级的功能（“好友”按钮）
    增加了BGM（“作战”按钮）

--- js/button.js
+++ js/button.js
@@ -1,12 +1,32 @@
 // JavaScript Document
-function info(){
-	var stat = document.getElementById("infoBox").className;
-	if (stat == "container")
-	{
-		document.getElementById("infoBox").className = "hidden";
+function hide(obj){
+	var stat = document.getElementById(obj).className;
+	switch(stat){
+		case "hidden":
+			document.getElementById(obj).className = "container";
+			break;
+		case "container":
+			document.getElementById(obj).className = "hidden";
+			break;
 	}
-	else
-	{
-		document.getElementById("infoBox").className = "container";
+	console.log("click");
+}
+
+function user(){
+	document.getElementById("username").innerHTML = document.getElementById("new_username").value;
+	document.getElementById("userid").innerHTML = "ID："+document.getElementById("new_userid").value;
+	document.getElementById("level-num").innerHTML = document.getElementById("new_userlv").value;
+	console.log(typeof document.getElementById("new_userlv").value);
+	hide("settingBox");
+}
+
+function bgm(){
+	var audio = document.getElementById('lifestream');
+	if(audio.paused){                 
+		audio.play();
 	}
+	else{
+		audio.pause();
+	}
+	console.log("bgm");
 }
