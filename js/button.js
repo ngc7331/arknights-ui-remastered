c8cca6fc6cde920608823992a67cff499b3354f5
ngc7331 <ngc7331@outlook.com>
2020-04-09 20:15:32 +0800

    V0.3
    增加了选择角色立绘的功能
    微调立绘位置以尽可能多的匹配干员

--- js/button.js
+++ js/button.js
@@ -9,24 +9,37 @@ function hide(obj){
 			document.getElementById(obj).className = "hidden";
 			break;
 	}
-	console.log("click");
+	//console.log("click");
 }
 
 function user(){
 	document.getElementById("username").innerHTML = document.getElementById("new_username").value;
 	document.getElementById("userid").innerHTML = "ID："+document.getElementById("new_userid").value;
 	document.getElementById("level-num").innerHTML = document.getElementById("new_userlv").value;
-	console.log(typeof document.getElementById("new_userlv").value);
+	document.getElementById("battlelv").innerHTML = document.getElementById("new_battlelv").value;
+	document.getElementById("money").innerHTML = document.getElementById("new_money").value;
+	document.getElementById("jasper").innerHTML = document.getElementById("new_jasper").value;
+	document.getElementById("stone").innerHTML = document.getElementById("new_stone").value;
+	document.getElementById("dialog").innerHTML = document.getElementById("new_dialog").value;
 	hide("settingBox");
 }
 
 function bgm(){
-	var audio = document.getElementById('lifestream');
+	var audio = document.getElementById("lifestream");
 	if(audio.paused){                 
 		audio.play();
 	}
 	else{
 		audio.pause();
 	}
-	console.log("bgm");
+	//console.log("bgm");
+}
+
+function bgm_volume(num){
+	document.getElementById("lifestream").volume=num;
+}
+
+function set_char(obj){
+	document.getElementById("chara").src = "img/characters/char_"+obj+".png";
+	hide("charBox");
 }
