d9b606e61106c02b952ef3c850cbbf086269be54
ngc7331 <ngc7331@outlook.com>
2020-04-10 20:47:16 +0800

    V0.4
    1、补充了之前缺失的立绘并压缩了过大的文件，删除了不必要的图片
    2、增加了自定义理智上限的功能，并将理智作为BGM音量显示+控制入口
    3、增加了打开新弹窗时自动关闭其它弹窗的功能
    4、增加了一个没什么卵用的导出设置的功能，以“仓库”作为入口
    5、增加了BGM播放状态的提示
    6、一堆有的没的的小修改

--- js/button.js
+++ js/button.js
@@ -1,5 +1,5 @@
 // JavaScript Document
-function hide(obj){
+function hidden_switch(obj){
 	var stat = document.getElementById(obj).className;
 	switch(stat){
 		case "hidden":
@@ -9,7 +9,16 @@ function hide(obj){
 			document.getElementById(obj).className = "hidden";
 			break;
 	}
-	//console.log("click");
+	var boxes = ["infoBox","charBox","settingBox","exportBox","volumeBox"];
+	for (var i=0; i<boxes.length; i++){
+		if (boxes[i] != obj){
+			hide(boxes[i]);
+		}
+	}
+}
+
+function hide(obj){
+	document.getElementById(obj).className = "hidden";
 }
 
 function user(){
@@ -21,6 +30,9 @@ function user(){
 	document.getElementById("jasper").innerHTML = document.getElementById("new_jasper").value;
 	document.getElementById("stone").innerHTML = document.getElementById("new_stone").value;
 	document.getElementById("dialog").innerHTML = document.getElementById("new_dialog").value;
+	document.getElementById("figure_total").innerHTML = "\\"+document.getElementById("new_figure_total").value;
+	var volume = document.getElementById("lifestream").volume;
+	bgm_volume(volume*100);
 	hide("settingBox");
 }
 
@@ -28,18 +40,77 @@ function bgm(){
 	var audio = document.getElementById("lifestream");
 	if(audio.paused){                 
 		audio.play();
+		document.getElementById("battletext").innerHTML = "暂停";
 	}
 	else{
 		audio.pause();
+		document.getElementById("battletext").innerHTML = "播放";
 	}
-	//console.log("bgm");
 }
 
 function bgm_volume(num){
-	document.getElementById("lifestream").volume=num;
+	document.getElementById("lifestream").volume=num/100;
+	var total = parseInt(document.getElementById("figure_total").innerHTML.slice(1));
+	document.getElementById("figure_value").innerHTML=parseInt(num*total/100);
+}
+
+function mouseover(obj){
+	switch(obj){
+		case "bgm":
+			var audio = document.getElementById("lifestream");
+			if(audio.paused){                 
+				document.getElementById("battletext").innerHTML = "播放"
+			}
+			else{
+				document.getElementById("battletext").innerHTML = "暂停"
+			}
+			break;
+	}
+}
+
+function mouseout(obj){
+	switch(obj){
+		case "bgm":
+			document.getElementById("battletext").innerHTML = "作战"
+			break;
+	}
 }
 
 function set_char(obj){
 	document.getElementById("chara").src = "img/characters/char_"+obj+".png";
 	hide("charBox");
+}
+
+function update_vol_display(){
+	document.getElementById("vol_display").innerHTML = "当前音量："+document.getElementById("set_volume").value;
+}
+
+function export_to_obj(){
+	var character = document.getElementById("chara").src;
+	character = character.match("img\/characters\/char_.*")[0];
+	var obj = {
+		name:document.getElementById("username").innerHTML,
+		id:document.getElementById("userid").innerHTML,
+		lv:document.getElementById("level-num").innerHTML,
+		battlelv:document.getElementById("battlelv").innerHTML,
+		money:document.getElementById("money").innerHTML,
+		jasper:document.getElementById("jasper").innerHTML,
+		stone:document.getElementById("stone").innerHTML,
+		dialog:document.getElementById("dialog").innerHTML,
+		char:character,
+	}
+	document.getElementById("export_obj").value = JSON.stringify(obj);
+}
+
+function import_from_obj(){
+	var obj = JSON.parse(document.getElementById("import_obj").value);
+	document.getElementById("username").innerHTML = obj.name;
+	document.getElementById("userid").innerHTML = obj.id;
+	document.getElementById("level-num").innerHTML = obj.lv;
+	document.getElementById("battlelv").innerHTML = obj.battlelv;
+	document.getElementById("money").innerHTML = obj.money;
+	document.getElementById("jasper").innerHTML = obj.jasper;
+	document.getElementById("stone").innerHTML = obj.stone;
+	document.getElementById("dialog").innerHTML = obj.dialog;
+	document.getElementById("chara").src = obj.char;
 }
