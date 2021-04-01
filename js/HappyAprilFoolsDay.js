6edef281357acbe29e04a32eb58dd4d29503ff1e
ngc7331 <ngc7331@outlook.com>
2021-04-01 20:42:23 +0800

    Happy April Fools‘ Day！
    Happy April Fools‘ Day！

---
+++ js/HappyAprilFoolsDay.js
@@ -0,0 +1,11 @@
+var HappyAprilFoolsDay = function (m) {
+  if (m == 1) {
+    app.char_src = "img/characters/special_haimao_1.png"
+    return "很可怕吗，是的很可怕。"
+  } else if (m == 2) {
+    app.char_src = "img/characters/special_haimao_skin1.png"
+    return "だめだね　だめよ　だめなのよ。"
+  } else {
+    return "？"
+  }
+}
